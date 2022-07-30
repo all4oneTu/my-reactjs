2d0cdde712766cff5db8553dd4b20231d179838b
all4oneTu <81431586+all4oneTu@users.noreply.github.com>
2022-07-30 22:54:05 +0700

    update

--- src/components/WellcomePage/RegisterPage/Register.js
+++ src/components/WellcomePage/RegisterPage/Register.js
@@ -4,17 +4,20 @@ import { auth } from "../../database/firebase";
 function Register() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
   const handlerClick = (e) => {
     e.preventDefault();
     console.log(username);
     console.log(password);
-    createUserWithEmailAndPassword(auth, username, password).then(
-      (userCredential) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
         var user = userCredential.user;
         console.log("User created successfully");
         console.log(user);
-      }
-    );
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
   return (
     <div>
@@ -24,7 +27,7 @@ function Register() {
           placeholder="username"
           onChange={(e) => setUsername(e.target.value)}
         />
-        <input placeholder="email" />
+        <input placeholder="email" onChange={(e) => setEmail(e.target.value)} />
         <input
           placeholder="password"
           onChange={(e) => setPassword(e.target.value)}
